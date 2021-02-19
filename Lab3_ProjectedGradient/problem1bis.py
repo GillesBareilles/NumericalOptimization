2a1af799e843a27dfada87b08c4562456cb98d9f
Gilles Bareilles <gilles.bareilles@protonmail.com>
2021-02-19 15:10:29 +0100

    update lab3

--- Lab3_ProjectedGradient/problem1bis.py
+++ Lab3_ProjectedGradient/problem1bis.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Problem 21
-# 
-# 
-# The objective of Problem 21 is to minimize a simple quadratic function $f$ on $\mathbb{R}^2$, constrained to $x\ge 0$: 
-# 
+# # Problem 1bis
+#
+#
+# The objective of Problem 1bis is to minimize a simple quadratic function $f$ on $\mathbb{R}^2$:
+#
 # $$\begin{array}{rrcll}
 # f: & \mathbb{R}^2 & \to &\mathbb{R}\\
 # & (x_1,x_2) & \mapsto  & 4 (x_1-1)^2 + 2(x_2+0.5)^2
 # \end{array}$$
-# <center><img src="Fig/1.png" width="50%"></center>
 
-# ### Function definition 
-
-# In[1]:
 
 
 ##### Function definition
@@ -25,9 +21,9 @@ def f(x):
 ####
 
 ##### Plot parameters f
-x1_min = -4.
-x1_max = 3.
-x2_min = -4.
+x1_min = -1.
+x1_max = 5.
+x2_min = -1.
 x2_max = 3.
 nb_points = 200
 vmin = 0
@@ -38,12 +34,11 @@ title = 'f: a simple function'
 
 
 # ### Some parameters
-# 
+#
 # Before solving things numerically, some useful things can be computed:
 # * Properties of $f$: lower bounds, Lipschitz constant of $\nabla f$, strong convexity constant, etc
 # * Good starting points (for hot starting e.g.)
 
-# In[2]:
 
 
 ###### Useful Parameters
@@ -51,13 +46,13 @@ L = 8        # Lipschitz constant of the gradient
 
 
 # ### Oracles
-# 
+#
 # Numerical optimization methods need callable *oracles* for properties of $f$, that is a function that, given a point $x$ in the domain of $f$, returns $f$ and/or gradient, Hessian of $f$ at point $x$. We talk about the *order* of an oracle as the number of differentiations given (0th order for just $f$, 1st order for the gradient, 2nd for gradient + Hessian).
-# 
-# > Observe the first order oracle `f_grad`.
-# 
 
-# In[3]:
+
+
+# Q: Observe the first order oracle `f_grad`.
+
 
 
 import numpy as np
@@ -66,15 +61,13 @@ import numpy as np
 def f_grad(x):
     x1 = x[0]
     x2 = x[1]
-    gx = 8*(x1-1) 
+    gx = 8*(x1-1)
     gy = 4*(x2+0.5)
     return np.array( [ gx  ,  gy  ] )
 ####
 
 
-# > Fill the following second order oracle `f_grad_hessian`.
-
-# In[4]:
+# Q: Observe the second order oracle `f_grad_hessian`.
 
 
 import numpy as np
@@ -83,10 +76,10 @@ import numpy as np
 def f_grad_hessian(x):
     x1 = x[0]
     x2 = x[1]
-    gx = 8*(x1-1) 
-    gy = 4*(x2+0.5) 
+    gx = 8*(x1-1)
+    gy = 4*(x2+0.5)
     g = np.array( [ gx  ,  gy  ] )
-    H = np.array(  [ ( 8.0 , 0 )  ,  ( 0 , 4.0 )  ]  )  ### -> To complete DONE 
+    H = np.array(  [ (8.0 , 0.0)  ,  ( 0.0, 4.0 )  ]  )
     return g,H
 ####
 
