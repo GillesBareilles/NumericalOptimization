7218bafb824ac0fdfe26eb27aeea2fa3a774f150
Gilles Bareilles <gilles.bareilles@protonmail.com>
2021-02-22 10:03:13 +0100

    add adaptive gradient function calls

--- Lab3_ProjectedGradient/algoProjGradient.py
+++ Lab3_ProjectedGradient/algoProjGradient.py
@@ -61,7 +61,43 @@ def interval_gradient_algorithm(f , f_grad , x0 , infbound , supbound , step , P
     for k in range(ITE_MAX):
 
         ## update iterate
-        # TODO: complete by gradient step projected onto the set "x >= 0"
+        # TODO: complete by gradient step projected onto the set "a <= x <= b"
+        g = f_grad(x)
+        x = x
+
+        ## Log current iterate
+        x_tab = np.vstack((x_tab,x))
+
+        ## Check stoping criterion
+        # TODO: update stopping criterion
+        if np.linalg.norm(g) < stop:
+           break
+
+    t_e =  timeit.default_timer()
+    print("FINISHED -- {:d} iterations / {:.6f}s -- final value: {:f} at point ({:.2f},{:.2f})\n\n".format(k,t_e-t_s,f(x),x[0],x[1]))
+    return x,x_tab
+
+
+
+### Adaptive stepsize projected gradient algorithm for interval constraints
+
+import numpy as np
+import timeit
+
+def adaptive_gradient_algorithm(f , f_grad , x0 , infbound , supbound , step , PREC , ITE_MAX ):
+    x = np.copy(x0)
+    stop = PREC*np.linalg.norm(f_grad(x))
+
+    epsilon = PREC*np.ones_like(x0)
+
+    x_tab = np.copy(x)
+    print("------------------------------------\n Adaptive Stepsize gradient\n------------------------------------\nSTART    -- stepsize = {:0}".format(step))
+    t_s =  timeit.default_timer()
+    for k in range(ITE_MAX):
+
+        ## update iterate
+        # TODO: complete by gradient step projected onto the set "a <= x <= b"
+        # TODO: find a "good" stepsize at each iteration
         g = f_grad(x)
         x = x
 
